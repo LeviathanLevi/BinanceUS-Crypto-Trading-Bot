7d3a152ae4f5c95bc5eb95a0ac712f176976f2db
LeviathanLevi <leutholdlevi@gmail.com>
2022-04-07 18:23:40 -0700

    Removed checking for the balance in exchange for asking for user input, sectioned the code out and added tradeData

--- Index.py
+++ Index.py
@@ -2,18 +2,23 @@ from ast import Not
 import os
 import asyncio
 import json
+import math
 from symtable import Symbol
 from operator import itemgetter
-
 from binance import AsyncClient, DepthCacheManager, BinanceSocketManager
 from dotenv import load_dotenv
 
+async def beginTrading(tradeData):
+    if tradeData['positionExists'] == False:
+        print("No position")
+    else:
+        print("position exists")
+
 async def main():
     # Get trading pair:
-    TRADESYMBOL = input("Enter the symbol you'd like to trade (ex: BTCUSD): ")
-    SELLPOSITIONDELTA = float(input("Enter the sell position delta (ex: .02): "))
-    BUYPOSITIONDELTA = float(input("Enter the buy position delta (ex: .015): "))
-    BASEAMOUNTTOTRADE = float(input("Enter the base currency amount to trade with (BTC ex: 0.00054): "))
+    TRADESYMBOL = input('Enter the symbol you\'d like to trade (ex: BTCUSD): ')
+    SELLPOSITIONDELTA = float(input('Enter the sell position delta (ex: .02): '))
+    BUYPOSITIONDELTA = float(input('Enter the buy position delta (ex: .015): '))
 
     # initialise the client
     client = await AsyncClient.create()
@@ -27,16 +32,20 @@ async def main():
 
     info = await client.get_account() # Fees: makerCommission and takerCommission 
 
-    #Remove this and let the user enter the trading amount?
-    availableQuoteBalance = None
-    quoteBalance = next((balance for balance in info['balances'] if balance['asset'] == symbolInfo['quoteAsset']), None)
-    if quoteBalance is not None:
-        availableQuoteBalance = float(quoteBalance['free'])
+    quoteTradeBalance = float(input('Enter the amount of {0} to trade with (BTC ex: 0.00054): '.format(symbolInfo['quoteAsset'])))
 
     tradeData = {
         'positionExists': False,
         'lastPeakPrice': None,
-        'lastValleyPrice': None
+        'lastValleyPrice': None,
+        'TRADESYMBOL': TRADESYMBOL,
+        'SELLPOSITIONDELTA': SELLPOSITIONDELTA,
+        'BUYPOSITIONDELTA': BUYPOSITIONDELTA,
+        'client': client,
+        'symbolInfo': symbolInfo,
+        'info': info,
+        'quoteTradeBalance': quoteTradeBalance,
+        'currentPrice': None
     }
     
     # initialise websocket factory manager
@@ -45,12 +54,10 @@ async def main():
     async with bsm.trade_socket(TRADESYMBOL) as ts:
 
         while True:
-            res = await ts.recv() # 'p'
-            print(f'recv {res}')
+            res = await ts.recv() 
+            tradeData['currentPrice'] = res['p']
+            await beginTrading(tradeData)
 
-    await client.close_connection()
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
